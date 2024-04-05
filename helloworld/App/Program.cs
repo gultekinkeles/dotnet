cdcbd4d24b9d457b7dd23c702ddff07e3b0ec2e2
gultekinkeles <gultekin.keles@konsalt.com.tr>
2024-04-05 16:06:39 +0300

    last

---
+++ helloworld/App/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+ 
+var counter = 0;
+ 
+while (true)
+{ 
+    Console.WriteLine($"Hello: {++counter}");
+    await Task.Delay(TimeSpan.FromMilliseconds(1_000));
+}
